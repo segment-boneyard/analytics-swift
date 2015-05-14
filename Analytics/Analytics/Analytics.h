765fac0fde09c59da35add1f79187c667552afd1
Prateek Srivastava <f2prateek@gmail.com>
2015-05-14 13:06:34 -0700

    Initial Commit

---
+++ Analytics/Analytics/Analytics.h
@@ -0,0 +1,19 @@
+//
+//  Analytics.h
+//  Analytics
+//
+//  Created by Prateek Srivastava on 2015-05-14.
+//  Copyright (c) 2015 Segment. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Analytics.
+FOUNDATION_EXPORT double AnalyticsVersionNumber;
+
+//! Project version string for Analytics.
+FOUNDATION_EXPORT const unsigned char AnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Analytics/PublicHeader.h>
+
+

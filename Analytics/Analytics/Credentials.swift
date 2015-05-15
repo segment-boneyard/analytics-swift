e47f32ec556d96abb726231089e35b7e7e0be7ca
Prateek Srivastava <f2prateek@gmail.com>
2015-05-15 14:44:00 -0700

    Add docs

--- Analytics/Analytics/Credentials.swift
+++ Analytics/Analytics/Credentials.swift
@@ -10,11 +10,13 @@ import Foundation
 
 class Credentials {
   
+  /** Returns this string encoded as Base64. */
   private static func base64(string: String) -> String {
     let utf8str = string.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
     return utf8str.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.Encoding64CharacterLineLength)
   }
   
+  /** Returns an auth credential for the Basic scheme. */
   static func basic(username: String, password: String) -> String {
     return String(format: "Basic %@", base64(String(format: "%@:%@", username, password)))
   }
